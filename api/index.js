047eb0bdb8b199d404ccba850e780bbdd4838cdc
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-05 16:51:57 +0100

    typing ./api/index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,4 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+const serverless = require('serverless-http')
+const app = require('../app')
 
-const postRoutes = require('./routes/postRoutes')
-
-const app = express()
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
-app.use(express.json())
-
-mongoose.connect(process.env.MONGO_URL)
-    .then(()=>console.log('MongoDB connected !'))
-
-app.use('/api/posts', postRoutes)
-
-// app.listen(5000, ()=> console.log('Server started on port 5000'))
-
-module.exports = app
+module.exports = serverless(app)
