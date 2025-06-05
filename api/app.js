a8a79ea687959453f6da7e5710d966b33cfb100f
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-05 15:31:46 +0100

    sdmflkbgn

---
+++ api/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const postRoutes = require('./routes/postRoutes')
+
+const app = express()
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log('MongoDB connected !'))
+
+app.use('/api/posts', postRoutes)
+app.listen(5000, ()=> console.log('Server started on port 5000'))
