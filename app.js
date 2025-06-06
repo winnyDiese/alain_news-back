9e2b042c8f39492726e989a472fd8fa707540fb7
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-06 07:52:47 +0100

    adding function connectDB to app.js file...

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(()=>console.log('MongoDB connected !'))
+// mongoose.connect(process.env.MONGO_URL)
+//     .then(()=>console.log('MongoDB connected !'))
+
+// connection to DB
+connectDB()
 
 app.use('/api/posts', postRoutes)
 
