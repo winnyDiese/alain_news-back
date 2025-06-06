03dcf935852792aa8a84706eb2876d0f39ffff7e
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-06 09:05:18 +0100

    sdlfvkjbni

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const serverless = require('serverless-http');
-const app = require('../app');
+// const serverless = require('serverless-http');
+// const app = require('../app');
 
-module.exports = serverless(app);
+// module.exports = serverless(app);
--- app.js
+++ app.js
@@ -9,10 +9,9 @@ require('dotenv').config()
 const postRoutes = require('./routes/postRoutes')
 
 const app = express()
-// app.use(cors({
-//   origin: 'http://localhost:3000'
-// }))
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
@@ -23,9 +22,9 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use('/api/posts', postRoutes)
 
-// app.listen(5000, ()=> console.log('Server started on port 5000'))
-if (require.main === module) {
-  app.listen(5000, () => console.log('Server running on port 5000'));
-}
+app.listen(5000, ()=> console.log('Server started on port 5000'))
+// if (require.main === module) {
+//   app.listen(5000, () => console.log('Server running on port 5000'));
+// }
 
 module.exports = app
