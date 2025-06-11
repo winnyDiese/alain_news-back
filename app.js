9274bf09098dae3156e159fd32f3b2e784219d68
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-11 08:53:55 +0100

    dmbveniun

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 
 
 const postRoutes = require('./routes/postRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 app.use(cors({
@@ -23,6 +24,7 @@ mongoose.connect(process.env.MONGO_URL)
 // connectDB()
 
 app.use('/api/posts', postRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(5000, ()=> console.log('Server started on port 5000'))
 // if (require.main === module) {
