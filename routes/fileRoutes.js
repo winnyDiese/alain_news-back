cbe01f24a5fbe3dd27b59cd954ca3ffa447f1bc2
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-17 10:46:57 +0100

    create file route...

---
+++ routes/fileRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Post = require('../models/Post');
+const upload = require('../lib/cloudinary'); // 👈 import upload
+const router = express.Router();
+
+router.post('/', upload.single('image'), async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const imageUrl = req.file.path; // ✅ URL Cloudinary
+
+    const newPost = new Post({
+      title,
+      content,
+      imageUrl,
+    });
+
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
